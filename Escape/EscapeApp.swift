5325bd62eb03fb7abdd652a067647237383dfd10
Emily Hoppe <hoppe23@up.edu>
2021-04-14 10:52:05 -0700

    Initial Commit

---
+++ Escape/EscapeApp.swift
@@ -0,0 +1,17 @@
+//
+//  EscapeApp.swift
+//  Escape
+//
+//  Created by Maria Sepulveda on 4/14/21.
+//
+
+import SwiftUI
+
+@main
+struct EscapeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
